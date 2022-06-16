73441427c2b43c9f0ab4b8253ba853607c683cd5
TNTX-3 <tinaxie3659@gmail.com>
2022-06-16 16:34:55 +1000

    star loops

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -78,7 +78,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+    return star_list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
